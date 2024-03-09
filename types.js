b36c0d191d54ff48a7eb20d40daf3a2aeb41e01b
meheraz1100 <meheraz1100@gmail.com>
2024-03-09 20:56:28 +0600

    primitive and non-primitive

---
+++ types.js
@@ -0,0 +1,27 @@
+//  JavaScript is a dynamic programming language
+
+// primitive types
+const a = 5;
+const b = 'Samsu kopay na akhon ar';
+const d = true;
+
+
+
+// non-primitive types
+const ages = [45, 65, 28];
+const student = { id: 23, class: 7};
+
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+
+
+let x = 5;
+let y = x;
+
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = { job: 'web developer'};
+let q = p;
+q = { job: 'Back end'}
+console.log(p, q);
